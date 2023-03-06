78e8c8f2b116e28c0bb9419bb9e9991e7011d780
Przemek <owca.przemek@hotmail.com>
2023-03-06 15:41:53 +0100

    added profile page

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -6,10 +6,11 @@ import Posts from "pages/widgets/Posts";
 import FriendList from "pages/widgets/FriendList";
 import Advert from "pages/widgets/Advert";
 import { useSelector } from "react-redux";
-const Home = () => {
+const Home = ({ isProfile = false }) => {
   const user = useSelector((state) => state.user);
   const { _id, imgPath } = user;
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
   return (
     <Box>
       <Navbar />
@@ -17,20 +18,25 @@ const Home = () => {
         width="100%"
         padding="2rem 6%"
         display={isNonMobileScreens ? "flex" : "block"}
-        gap="0.5rem"
-        justifyContent="space-between"
+        gap={isProfile ? "5rem" : "1rem"}
+        justifyContent={isProfile ? "center" : "space-between"}
       >
         <Box flexBasis={isNonMobileScreens ? "25%" : undefined}>
           <User userId={_id} imgPath={imgPath} />
+          {isProfile && <FriendList userId={_id} />}
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "45%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <Post imgPath={imgPath} />
-          <Posts userId={_id} />
+          {isProfile ? (
+            <Posts userId={_id} isProfile={isProfile} />
+          ) : (
+            <Posts userId={_id} />
+          )}
         </Box>
-        {isNonMobileScreens && (
+        {isNonMobileScreens && !isProfile && (
           <Box flexBasis="25%">
             <Advert />
             <FriendList userId={_id} />
